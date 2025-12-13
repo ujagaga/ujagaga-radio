e4eca8267fc25b052635b684bd2daae4f0d1d04d
Rada Berar <ujagaga@gmail.com>
2025-12-13 23:23:03 +0100

    test

--- show_ip.py
+++ show_ip.py
@@ -5,6 +5,9 @@ import socket
 import fcntl
 import struct
 
+lcd = LCD1602(i2c_addr=0x27, i2c_bus=0)
+ip_message = ""
+
 def get_wifi_ip(iface="wlan0"):
     """
     Returns IPv4 address of the given interface, or None if no IP is assigned.
@@ -13,29 +16,39 @@ def get_wifi_ip(iface="wlan0"):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         ifreq = struct.pack("256s", iface.encode("utf-8")[:15])
         res = fcntl.ioctl(sock.fileno(), 0x8915, ifreq)  # SIOCGIFADDR
-        ip = socket.inet_ntoa(res[20:24])
-        return ip
+        return socket.inet_ntoa(res[20:24])
     except OSError:
         return None
 
 
-lcd = LCD1602(i2c_addr=0x27, i2c_bus=0)
+def lcd_write(message = "", cursor_pos = 0):
+    try:
+        lcd.LCD_SetCursor(cursor_pos)
+        lcd.LCD_Write(message)
+    finally:
+        pass
 
-ip = get_wifi_ip("wlan0")
-if ip:
-    ip_message = f"IP: {ip}"
-    print("WiFi IP:", ip)
-else:
-    ip_message = "No WiFi IP"
-    print("No WiFi IP assigned")
 
-try:
-    lcd.LCD_init()
-    lcd.LCD_Backlight(True)
+def init():
+    global ip_message
 
-    lcd.LCD_SetCursor(0)
-    lcd.LCD_Write(ip_message)
+    ip = get_wifi_ip("wlan0")
+    if ip:
+        ip_message = f"{ip}"
+    else:
+        ip = get_wifi_ip("eth0")
+        if ip:
+            ip_message = f"{ip}"
+        else:
+            ip_message = "No WiFi IP"
 
-finally:
-    lcd.close()
+    try:
+        lcd.LCD_init()
+        lcd.LCD_Backlight(True)
+    finally:
+        pass
 
+init()
+lcd_write(ip_message, 0)
+lcd_write("test", 16)
+lcd.close()
